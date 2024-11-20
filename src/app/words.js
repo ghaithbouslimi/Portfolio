4ed9c9ddd576e930c50090111f4861a6fe18cb00
Bouslimi Ghaith <101145229+ghaithbouslimi@users.noreply.github.com>
2024-11-20 23:01:08 +0100

    personal

---
+++ src/app/words.js
@@ -0,0 +1,26 @@
+ /*
+Innovative – creative and original in thinking.
+Influential – having the power to affect or change others.
+Intrepid – fearless and adventurous.
+Inspiring – filling someone with the urge to do or feel something positive.
+Impressive – evoking admiration or respect.
+Indomitable – impossible to subdue or defeat.
+Invaluable – extremely useful or indispensable.
+Invincible – too powerful to be defeated or overcome.
+Insightful – having a deep understanding or clear perception.
+Impeccable – in accordance with the highest standards of propriety; faultless.
+ */
+
+
+
+
+/*Brave – showing courage in the face of danger.
+Bold – willing to take risks; confident and courageous.
+Brilliant – exceptionally intelligent or talented.
+Balanced – showing good judgment and stability.
+Blazing – intensely powerful or impressive.
+Boundless – unlimited; having no boundaries.
+Benevolent – well-meaning and kindly.
+Brazen – bold and without shame.
+Breathtaking – astonishing or awe-inspiring in quality or impact.
+Ballistic – relating to extreme force or energy; explosive in intensity.*/
